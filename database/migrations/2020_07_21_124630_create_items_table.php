4228d10263bd5ae6b65dd77163f317743558c6ee
Ahmed Adisa <adisa.ahmed881@gmail.com>
2020-07-21 16:01:55 +0100

    Item, Transaction, Brand, Type migration

---
+++ database/migrations/2020_07_21_124630_create_items_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('description');
+            $table->integer('types_id');
+            $table->integer('brand_id');
+            $table->decimal('purchasedPrice');
+            $table->integer('balance');
+            $table->string('itemStoreLocation');
+            $table->decimal('priceItWillBeSold');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
